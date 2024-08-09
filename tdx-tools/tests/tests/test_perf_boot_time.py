7092f9425734dab1eed1378ad4e0c15bce50c99f
Jinen Gandhi <jinen.gandhi@intel.com>
2024-08-08 22:24:32 -0700

    Enbaling boot time tests in pycloudstack

---
+++ tdx-tools/tests/tests/test_perf_boot_time.py
@@ -0,0 +1,43 @@
+"""
+This module provide the case to test VM boot with max vCPU
+
+"""
+
+import logging
+import psutil
+import pytest
+from pycloudstack.vmparam import VM_TYPE_LEGACY, VM_STATE_RUNNING, VM_TYPE_EFI, VM_TYPE_TD, VMSpec
+from pycloudstack.util import timeit
+
+__author__ = 'cpio'
+
+LOG = logging.getLogger(__name__)
+
+
+# pylint: disable=invalid-name
+pytestmark = [
+    pytest.mark.vm_image("latest-guest-image"),
+    pytest.mark.vm_kernel("latest-guest-kernel"),
+]
+# Remving VMSpec.model_migtd() as it is consistently failing to get IP address.
+# Commenting the test out until we find a solution to the the IP address issue.
+@pytest.mark.parametrize("vm_type", [VM_TYPE_TD, VM_TYPE_EFI, VM_TYPE_LEGACY])
+@pytest.mark.parametrize("vmspec", [VMSpec.model_large(), VMSpec.model_base(), VMSpec.model_numa()])
+def test_boot_time(vm_factory, vm_ssh_pubkey, vm_type, vmspec):
+    """
+    Test boot TD guest with max vcpu KVM supports
+    """
+
+    LOG.info("Create guest")
+    inst = vm_factory.new_vm(vm_type, vmspec)
+    # customize the VM image
+    inst.image.inject_root_ssh_key(vm_ssh_pubkey)
+    inst.create()
+    def boot_vm_and_wait_ssh():
+        inst.start()
+        inst.wait_for_ssh_ready()
+    
+    timeit(boot_vm_and_wait_ssh)()
+    # Destroy VM to release CPU resource
+    inst.destroy()
+
